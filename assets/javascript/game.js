43871d4bd9fc0584e72e4161e35e7c516486f091
ahydorn <adamhydorn@gmail.com>
2019-05-28 22:25:31 -0700

    Reset Current score: value to 0 upon win/lose instead of keeping it until the first  click of NG+

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,17 @@ var previous = 0;
 
 var newRound = function() {
     //  Dump assigned crystal numbers
+
     $(".crystals").empty();
+
+    // Reset "Current score:" value to 0 instead of keeping it until the next click.
+    $("#current").html('Your current score: ' + 0);
 
     //  A random target number BETWEEN (and including, I think) 19-120 is generated at the start of each game (Math.random())
     randomTarget = Math.floor(Math.random() * 102) + 19;
     $("#target").html('Your target: ' + randomTarget);
 
-    //  Need to make 4 crystals each with values between (and including, I think) 1-12
+    //  Create 4 crystals, each with values between (and including, I think) 1-12
     for (var i = 0; i < 4; i++) {
         var random = Math.floor(Math.random() * 12) + 1;
         var crystal = $("<div>");
@@ -32,10 +36,16 @@ newRound();
 
 //  When the player clicks on a crystal, it will add a specific amount of points to the players score
 $(document).on('click', ".crystal", function() {
+
     var num = parseInt($(this).attr('data-random'));
 
     // Incriment running score by value of last clicked crystal
     previous += num;
+
+    // Display current score
+    var currentScore = previous;
+    $("#current").html('Your current score: ' + currentScore);
+
     // If running score > target score...
     if (previous > randomTarget) {
         // Incriment losses counter by 1
