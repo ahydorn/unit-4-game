d7f44edb9acbeab3cbe1773eb98986ea96fa0432
ahydorn <adamhydorn@gmail.com>
2019-05-29 09:40:34 -0700

    More JS comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var losses = 0;
 var wins = 0;
 var previous = 0;
 
+// Function to run at start of each round or new game
 var newRound = function() {
 
     //  Dump assigned crystal numbers
@@ -17,17 +18,23 @@ var newRound = function() {
 
     //  A random target number BETWEEN (and including, I think) 19-120 is generated at the start of each game (Math.random())
     randomTarget = Math.floor(Math.random() * 102) + 19;
+
+    // Add target number to HTML
     $("#target").html('Your target: ' + randomTarget);
 
     //  Create 4 crystals, each with values between (and including, I think) 1-12
+    // Create variable i, run loop as long as i < 4, incriment i by 1.
     for (var i = 0; i < 4; i++) {
+        // Define random as random number between 1 and 12 ((max-min+1)+1).
         var random = Math.floor(Math.random() * 12) + 1;
+        // Add div on each loop
         var crystal = $("<div>");
+        // Give crystal attributes of class: crystal and data-random: random.
         crystal.attr({
             "class": 'crystal',
             "data-random": random
         });
-
+        // Append crystal var to crystals class
         $(".crystals").append(crystal);
     }
 }
@@ -45,6 +52,8 @@ $(document).on('click', ".crystal", function() {
 
     // Display current score
     var currentScore = previous;
+
+    // Add current score counter to html with "current" id
     $("#current").html('Your current score: ' + currentScore);
 
     // If running score > target score...
@@ -55,6 +64,8 @@ $(document).on('click', ".crystal", function() {
 
         // Display current losses
         $("#losses").html("Losses: " + losses);
+
+        // Set previous variable to 0
         previous = 0;
 
         // And alert player of loss
@@ -69,10 +80,10 @@ $(document).on('click', ".crystal", function() {
         // Incriment wins counter by 1
         wins++;
 
-        // Display current wins.
+        // Display current wins
         $("#wins").html("Wins: " + wins);
 
-        // Set previous to 0 so it can incriment by 1
+        // Set previous to 0
         previous = 0;
 
         // And alert player of win
