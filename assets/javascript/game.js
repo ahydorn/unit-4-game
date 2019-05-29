bcd41620d89d646c1425887021358c073bb670f8
ahydorn <adamhydorn@gmail.com>
2019-05-28 19:39:04 -0700

    Added pseudo-code to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Pseudo-code
+
+//  A random target number BETWEEN 19-120 is selected at the start of each game
+
+//  Need to make 4 crystals
+
+//  Each crystal has random number BETWEEN 1-12 assigned to it at start of game
+
+//  When the player clicks on a crystal, it will add a specific amount of points to the players score
+
+//  The player wins if their score matches the randomly selected target number
+
+// The player triggers a loss when their score > than the randomly selected target number
+
+// The game shows the number of wins and losses
