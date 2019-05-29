733e2d432265877172309d3812b6cbfb697e31bc
ahydorn <adamhydorn@gmail.com>
2019-05-28 21:58:00 -0700

    Working skeleton of a game. No crystal values displayed yet, but win/lose functions working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,73 @@
-// Pseudo-code
+/*==============*/
+/*    Varbox    */
+/*==============*/
 
-//  A random target number BETWEEN 19-120 is selected at the start of each game
+var target;
+var losses = 0;
+var wins = 0;
+var previous = 0;
 
-//  Need to make 4 crystals
+var newRound = function() {
+    //  Dump assigned crystal numbers
+    $(".crystals").empty();
 
-//  Each crystal has random number BETWEEN 1-12 assigned to it at start of game
+    //  A random target number BETWEEN (and including, I think) 19-120 is generated at the start of each game (Math.random())
+    randomTarget = Math.floor(Math.random() * 102) + 19;
+    $("#target").html('Your target: ' + randomTarget);
+
+    //  Need to make 4 crystals each with values between (and including, I think) 1-12
+    for (var i = 0; i < 4; i++) {
+        var random = Math.floor(Math.random() * 12) + 1;
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random
+        });
+
+        $(".crystals").append(crystal);
+    }
+}
+
+newRound();
+
+var reset = function() {}
 
 //  When the player clicks on a crystal, it will add a specific amount of points to the players score
+$(document).on('click', function() {
+    var num = parseInt($(this).attr('data-random'));
+
+    // Incriment running score by value of last clicked crystal
+    previous += num;
+    // If running score > target score...
+    if (previous > randomTarget) {
+        // Incriment losses counter by 1
+        losses++;
+        $("#losses").html("Losses: " + losses);
+
+        previous = 0;
+
+        // And alert player of loss
+        alert("You lost!!");
+
+        // If player running score == target score exactly...
+    } else if (previous === randomTarget) {
+        // Incriment wins counter by 1
+        wins++;
+        $("#wins").html("Wins: " + wins);
+
+        previous = 0;
+
+        // And alert player of win
+        alert("You won!!");
+    };
+
+});
+
+
+/*==============================*/
+/*    Pseudo-code fragments     */
+/*==============================*/
+
 
 //  The player wins if their score matches the randomly selected target number
 
